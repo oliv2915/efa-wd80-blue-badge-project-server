80db876c782a2bafb8a3dd5bd6460742b9365182
Brian Oliver II <brian.d.oliver@icloud.com>
2021-04-21 22:03:05 -0400

    Added validation checks to UserModel and RecipeModel. Each check should return a custom message

--- models/userModel.js
+++ models/userModel.js
@@ -5,24 +5,66 @@ const User = db.define("user", {
     username: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
+        validate: {
+            notEmpty: {
+                args: true,
+                msg: "Username is required"
+            },
+            min: {
+                args: 4,
+                msg: "Username must be a minimum of 4 characters long"
+            }
+        }
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
+        validate: {
+            notEmpty: {
+                args: true,
+                msg: "Email address is required"
+            },
+            isEmail: {
+                args: true,
+                msg: "Must provide a proper email address"
+            }
+        }
     },
     password: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                args: true,
+                msg: "Password is required"
+            },
+            min: {
+                args: 6,
+                msg: "Password must be a minimum of 6 characters long"
+            }
+        }
     },
     firstName: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                args: true,
+                msg: "First Name can not be empty"
+            }
+        }
     },
     lastName: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                args: true,
+                msg: "Last Name can not be empty"
+            }
+        }
     },
     
 },);
