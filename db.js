7a2b680de8ee57e2eedfde04ae0e961fcfa4f82a
Brian Oliver II <brian.d.oliver@icloud.com>
2021-04-26 16:50:41 -0400

    deployment

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const {Sequelize} = require("sequelize");
 const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres"
+    dialect: "postgres",
+    ssl: true
 });
 module.exports = db;
