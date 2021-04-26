2bf76fa8aa6a1bc977929f9ac499e7fbe3410620
Brian Oliver II <brian.d.oliver@icloud.com>
2021-04-26 16:19:33 -0400

    added ssl flag to db connection

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
 const {Sequelize} = require("sequelize");
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL, {
+    ssl: true
+});
 module.exports = db;
